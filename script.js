15a6615a3031a2c9fe5c915b90b35bc15734ba17
metaxuan <adwin07939@icloud.com>
2025-06-25 23:44:31 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function sendTokens() {
-    const recipient = document.getElementById('recipient').value;
-    const amount = document.getElementById('amount').value;
-    document.getElementById('status').textContent = `Sending ${amount} tokens to ${recipient}...`;
-}
